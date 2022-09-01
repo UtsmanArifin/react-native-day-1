6036289b219e5da8f54241e4f4c10cc447485b37
Utsman Arifin <utsmanarifin@mail.ugm.ac.id>
2022-09-01 17:25:49 +0700

    DONE

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginPage from './src/features/Login/LoginPage';
+import WelcomePage from './src/features/Welcome/WelcomePage';
+import { ThemeProvider } from './src/shared/context/ThemeContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      {/* <WelcomePage/> */}
+      <LoginPage/>
+    </ThemeProvider>
+    // <LoginPage/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/features/Welcome/WelcomePage.js
@@ -0,0 +1,41 @@
+import { ImageBackground, StyleSheet, Text, Touchable, TouchableOpacity, View } from "react-native"
+import MainContainer from "../../shared/components/MainContainer"
+import LottieView from "lottie-react-native"
+import AppBackground from "../../shared/components/AppBackground"
+import FormButton from "../../shared/components/FormButton"
+import TitleLabel from "../../shared/components/TitleLabel"
+
+const WelcomePage = () =>{
+    return (
+        <MainContainer>
+            <AppBackground style={{
+                justifyContent: 'space-evenly',
+                alignItems: 'center'
+            }}>
+                <LottieView 
+                    autoPlay 
+                    style={styles.image} 
+                    source={require('../../../assets/img/116822-transfer-files.json')}/>
+
+                <View style={styles.titleContainer}>
+                    <TitleLabel text = 'POS System'/>
+                    <TitleLabel subTitle text='Simple Point of Sales'/>
+                </View>
+                <FormButton label='Sign In' onClick={()=>{}}></FormButton>
+            </AppBackground>
+        </MainContainer>
+    )
+}
+
+const styles = StyleSheet.create({
+    image : {
+        width : 200,
+        height : 200,
+        alignItems : "center"
+    },
+    titleContainer : {
+        alignItems : "center"
+    }
+})
+
+export default WelcomePage;
